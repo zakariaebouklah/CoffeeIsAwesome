f389eb43d68980ce206edcedf14a529b08a21438
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-10-31 23:07:53 +0100

    Remembering a state.

--- app/src/main/java/com/zakib/coffeeisgood/MainActivity.kt
+++ app/src/main/java/com/zakib/coffeeisgood/MainActivity.kt
@@ -11,7 +11,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,19 +48,28 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun FirstComposable(): Unit {
+    /**
+     * Remember, it's just a call where we can ask the framework to remember that value.
+     * Remember will actually act like it's similar to memo in other frameworks.
+     *
+     * PS: we can remember states as well as plain values
+     */
+    var name = remember {
+        mutableStateOf("Zakariae")
+        /**
+         * like in React we create a state(s) for the composable
+         * So I need to trigger a change within a state and we can do that but in this case we need to call mutableStateOf.
+         * In this case we are setting that is going to be the state of these composable.
+         * So that means that if the variable changes the state of the composable changes,
+         * and it's going to trigger a re-render.
+          */
+
+    }
     Row { // a column is used like a container to hold different composables.
-        Text(
-            text= "Hi I'm Zaki...",
-            modifier = Modifier
-                .background(Color.Green)
-                .padding(16.dp)
-        )
-        Text(
-            text= "Hi I'm a soft-dev engineer....",
-            modifier = Modifier
-                .background(Color.Gray)
-                .padding(16.dp)
-        )
+        Text("Hi My Name is ${name.value}")
+        TextField(value = name.value, onValueChange = {
+            name.value= it
+        })
     }
 }
 
