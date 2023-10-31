d456aae0d31643843c8d73ca8090449aef83a5d6
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-10-31 22:41:57 +0100

    Making Our First composable.

--- app/src/main/java/com/zakib/coffeeisgood/MainActivity.kt
+++ app/src/main/java/com/zakib/coffeeisgood/MainActivity.kt
@@ -3,13 +3,17 @@ package com.zakib.coffeeisgood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zakib.coffeeisgood.ui.theme.CoffeeIsGoodTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +26,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    Text("Hiii !!!")
                 }
             }
         }
     }
 }
 
+/**
+ * creating custom composable
+ * 'Text' is a built-in composable
+ */
+//@Preview
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FirstComposable(): Unit {
+    Text(
+        text= "Hi I'm Zaki...",
+        // order matters
+//        modifier = Modifier.padding(16.dp).background(Color.Green)
+        modifier = Modifier.background(Color.Green).padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true)
+@Preview(widthDp = 150)
 @Composable
-fun DefaultPreview() {
-    CoffeeIsGoodTheme {
-        Greeting("Android")
-    }
+fun SecondComposable(): Unit {
+    Text(text= "Hi I'm Zaki...")
 }
+
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    CoffeeIsGoodTheme {
+//        Greeting("Android")
+//    }
+//}
