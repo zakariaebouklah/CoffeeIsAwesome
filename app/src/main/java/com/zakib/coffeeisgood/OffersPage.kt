c2d6a6356b686e499255d889d3839efd772088fb
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 15:50:13 +0100

    Adding a Spacer composable and more styling to children composables.

--- app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
+++ app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
@@ -2,6 +2,8 @@ package com.zakib.coffeeisgood
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -16,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun Offer() {
 
@@ -37,12 +39,17 @@ fun Offer() {
         Text(
             text = "My title",
             style = MaterialTheme.typography.h1,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.background(Color.Gray).padding(16.dp),
+            fontWeight = FontWeight.Medium
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = "Description",
             style = MaterialTheme.typography.h3,
-            modifier = Modifier.padding(16.dp).background(Color.Green)
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(16.dp)
+                .background(Color.Green)
         )
     }
 }
