677f610360097f4be5e0b29a16827c710ee3c46c
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 15:40:39 +0100

    Adding Modifiers to style container and its children.

--- app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
+++ app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
@@ -1,14 +1,18 @@
 package com.zakib.coffeeisgood
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -24,8 +28,19 @@ fun Offer() {
      * So the idea is that we can go and use all the semantic styles in the theme
      * and try to keep working there instead of hard coding.
      */
-    Column {
-        Text(text = "My title", style = MaterialTheme.typography.h1)
-        Text(text = "Description", style = MaterialTheme.typography.h3)
+    Column (
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+            ){
+        Text(
+            text = "My title",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = "Description",
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
