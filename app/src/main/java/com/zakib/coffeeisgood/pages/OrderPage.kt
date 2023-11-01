f6fa9acf63394e91f333442e2500a5eab8f3e4b4
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 19:38:39 +0100

    Adding Rest of pages.

---
+++ app/src/main/java/com/zakib/coffeeisgood/pages/OrderPage.kt
@@ -0,0 +1,10 @@
+package com.zakib.coffeeisgood.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun OrderPage() {
+
+}
