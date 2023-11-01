92c1aaf27eeb914f9d9e8b86f3b2f8863331c3aa
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 15:57:40 +0100

    Selecting color palette for our app.

--- app/src/main/java/com/zakib/coffeeisgood/ui/theme/Color.kt
+++ app/src/main/java/com/zakib/coffeeisgood/ui/theme/Color.kt
@@ -6,3 +6,23 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+/**
+ * Selecting color palette for our app.
+ *
+ * Note: unlike normal hex codes in css for e.g.
+ *       we use this format to represent the colors code :
+ *       (ARGB) :
+ *              A: alpha (FF = 1 (fully visible), 00 = 0 (fully transparent))
+ *              R : red
+ *              G : green
+ *              B : blue
+ */
+
+val Primary = Color(0xFF43281C)
+val Secondary = Color(0xFF333D29)
+val Ternary = Color(0xFF7F4F24)
+val Alternative1 = Color(0xFFB08968)
+val Alternative2 = Color(0xFFDDB892)
+val CardBackground = Color(0xFFEDE0D4)
+val SurfaceBackground = Color(0xFFEFEFEF)
