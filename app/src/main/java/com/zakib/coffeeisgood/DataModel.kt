414df2c72dccbd91ff8f9224725b3bb2f250f25a
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 19:47:19 +0100

    Create a data model to render data on pages.

---
+++ app/src/main/java/com/zakib/coffeeisgood/DataModel.kt
@@ -0,0 +1,10 @@
+package com.zakib.coffeeisgood
+
+class Product(var id: Int, var name: String, price: Double, var image: String) {
+
+    val imageUrl get() = "https://firtman.github.io/coffeemasters/api/images/${this.image}"
+}
+
+class Category(var name: String, products: MutableList<Product>)
+
+class ItemInCart(var product: Product, quantity: Int)
