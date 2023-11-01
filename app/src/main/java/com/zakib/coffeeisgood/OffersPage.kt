bce9dd102a1ba41fcfe05192097312030a7627b5
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 16:28:48 +0100

    Styling the Asset.

--- app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
+++ app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
@@ -2,41 +2,41 @@ package com.zakib.coffeeisgood
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.zakib.coffeeisgood.ui.theme.Alternative1
 import com.zakib.coffeeisgood.ui.theme.Alternative2
+import com.zakib.coffeeisgood.ui.theme.Primary
 
 @Preview(showBackground = true)
 @Composable
 fun Offer() {
 
-    val style = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    )
+    /*
+        val style = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.W500,
+            fontSize = 14.sp
+        )
+     */
 
     /**
      * using the background_pattern svg
      */
     Image(
         painter = painterResource(R.drawable.background_pattern),
-        contentDescription = "Coffee Pattern"
+        contentDescription = "Coffee Pattern",
+        contentScale = ContentScale.FillWidth,
+        modifier = Modifier.fillMaxWidth()
     )
 
     /**
@@ -53,7 +53,7 @@ fun Offer() {
             style = MaterialTheme.typography.h1,
             // using the theme colors
             modifier = Modifier
-                .background(MaterialTheme.colors.primary)
+                .background(Primary)
                 .padding(16.dp),
             fontWeight = FontWeight.Medium
         )
