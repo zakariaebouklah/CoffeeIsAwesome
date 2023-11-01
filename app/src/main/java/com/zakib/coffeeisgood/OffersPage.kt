428712b9c388715844687bd313900f00964b2622
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-01 15:34:15 +0100

    Using 'MaterialTheme' to style things.

--- app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
+++ app/src/main/java/com/zakib/coffeeisgood/OffersPage.kt
@@ -1,6 +1,7 @@
 package com.zakib.coffeeisgood
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
@@ -19,8 +20,12 @@ fun Offer() {
         fontSize = 14.sp
     )
 
+    /**
+     * So the idea is that we can go and use all the semantic styles in the theme
+     * and try to keep working there instead of hard coding.
+     */
     Column {
-        Text(text = "My title", fontSize = 16.sp)
-        Text(text = "Description", style = style)
+        Text(text = "My title", style = MaterialTheme.typography.h1)
+        Text(text = "Description", style = MaterialTheme.typography.h3)
     }
 }
