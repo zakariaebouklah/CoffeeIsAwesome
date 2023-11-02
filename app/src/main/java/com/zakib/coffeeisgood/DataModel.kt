916297887593aa1084e03c156d43f13f745f7ae4
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-03 00:32:48 +0100

    Fix Bugs.

--- app/src/main/java/com/zakib/coffeeisgood/DataModel.kt
+++ app/src/main/java/com/zakib/coffeeisgood/DataModel.kt
@@ -5,6 +5,6 @@ class Product(var id: Int, var name: String, var price: Double, var image: Strin
     val imageUrl get() = "https://firtman.github.io/coffeemasters/api/images/${this.image}"
 }
 
-class Category(var name: String, products: MutableList<Product>)
+class Category(var name: String, var products: MutableList<Product>)
 
-class ItemInCart(var product: Product, quantity: Int)
+class ItemInCart(var product: Product, var quantity: Int)
