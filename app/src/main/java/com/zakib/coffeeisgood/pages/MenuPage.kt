a2b3365dc705cd93300d315440dc93d3ff0a1088
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-11-03 01:08:34 +0100

    MenuPage Completed.

--- app/src/main/java/com/zakib/coffeeisgood/pages/MenuPage.kt
+++ app/src/main/java/com/zakib/coffeeisgood/pages/MenuPage.kt
@@ -1,38 +1,42 @@
 package com.zakib.coffeeisgood.pages
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.zakib.coffeeisgood.Product
 import androidx.compose.material.Text
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.material.Button
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.zakib.coffeeisgood.ui.theme.Alternative1
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
+import coil.compose.AsyncImage
 import com.zakib.coffeeisgood.DataManager
-import com.zakib.coffeeisgood.R
 import com.zakib.coffeeisgood.ui.theme.CardBackground
 
 @Composable
 fun MenuPage(dataManager: DataManager) {
     LazyColumn{
-        items(count = 7){
-            Card (
-                elevation = 2.dp,
-                shape = RoundedCornerShape(12.dp),
-                modifier = Modifier.background(CardBackground).padding(12.dp)
-            ){
-                ProductItem(product = Product(1, "Foo", 35.6, ""), onAdd = {})
+
+        items(dataManager.menu){
+            Text(it.name)
+            it.products.forEach{ p ->
+                Card (
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .background(CardBackground)
+                        .padding(12.dp)
+                ){
+                    ProductItem(product = p, onAdd = {dataManager.addToCart(p)})
+                }
             }
         }
     }
@@ -49,8 +53,8 @@ fun ProductItem(product: Product, onAdd: (Product)->Unit) {
             .background(Color.White)
     ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.black_coffee),
+        AsyncImage(
+            model = product.imageUrl,
             contentDescription = "Image for ${product.name}",
             contentScale = ContentScale.FillWidth,
             modifier = Modifier
